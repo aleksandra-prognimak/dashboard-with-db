3cbc3dcf5479f21662e81ba1e56775d916badc2c
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-11-22 14:41:18 +0200

    test heroku

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
