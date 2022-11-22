5e627454ba92a047bce289afb65d8ed864da8872
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-11-22 13:21:30 +0200

    create heroku

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,34 +1,5 @@
 import City from '../models/City.js';
 
-/*
-export const data = async (req, res) => {
-  try {
-    const { name, schools, kindergartens, universities } = req.body;
-
-    const isCity = await City.findOne({ name });
-
-    if (isCity) {
-      return res.json({ message: 'Город уже есть' });
-    }
-
-    const newCity = new City({
-      name,
-      schools,
-      kindergartens,
-      universities,
-    });
-
-    await newCity.save();
-
-    res.json({
-      newCity,
-      message: 'Город добавлен',
-    });
-  } catch (error) {
-    res.json({ message: 'что-то пошло не так' });
-  }
-}; */
-
 export const getData = async(req, res) => {
   try {
     const data = await City.find();
